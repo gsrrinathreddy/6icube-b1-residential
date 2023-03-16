485ae3ed4929c2bfe6badbb936020bc0ae34e417
nagasailikhitha <n.s.likhithach@gmail.com>
2023-03-16 16:24:33 +0530

    adding-navbar

--- likhita/demo/src/components/Navbar/index.js
+++ likhita/demo/src/components/Navbar/index.js
@@ -11,12 +11,20 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import { Link } from 'react-router-dom';
+import {  red } from '@mui/material/colors';
+import HomeIcon from '@mui/icons-material/Home';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import Favorite from '@mui/icons-material/Favorite';
+import Checkbox from '@mui/material/Checkbox';
 
 const pages = ['SkinCare', 'HairCare', 'MakeUp'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
-function Navbar() {
+function Navbar(props) {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
@@ -36,10 +44,10 @@ function Navbar() {
   };
 
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={{backgroundColor:"orange"}}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+        <HomeIcon sx={{ color: red[500], marginRight:"10px" }} />   
           <Typography
             variant="h6"
             noWrap
@@ -50,15 +58,15 @@ function Navbar() {
               display: { xs: 'none', md: 'flex' },
               fontFamily: 'monospace',
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
+              letterSpacing: '.1rem',
+              color: '#9c27b0',
               textDecoration: 'none',
             }}
           >
-            LOGO
+            Cosmetics
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none', p: 0  } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -94,7 +102,7 @@ function Navbar() {
               ))}
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+         
           <Typography
             variant="h5"
             noWrap
@@ -105,8 +113,8 @@ function Navbar() {
               display: { xs: 'flex', md: 'none' },
               flexGrow: 1,
               fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
+              fontWeight: 900,
+              letterSpacing: '.1rem',
               color: 'inherit',
               textDecoration: 'none',
             }}
@@ -115,6 +123,8 @@ function Navbar() {
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
+                <Link to={page} style={{textDecoration:"none"}}>
+                  
               <Button
                 key={page}
                 onClick={handleCloseNavMenu}
@@ -122,13 +132,33 @@ function Navbar() {
               >
                 {page}
               </Button>
+              </Link>
             ))}
           </Box>
 
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+       <Paper
+      component="form"
+      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 200,marginRight:"20px" }}
+    >
+      
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Search "
+        inputProps={{ 'aria-label': 'search ' }}
+      />
+      <IconButton type="button" sx={{ p: '1px' }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+      
+    </Paper>
+    <IconButton sx={{color:"red"}} aria-label="add to shopping cart">
+      <Favorite/>
+</IconButton>
+        <ShoppingCartIcon sx={{marginRight:"15px",color:"red"}} />
+              <Avatar alt="Remy Sharp" src="https://media.istockphoto.com/id/502129687/photo/decorative-cosmetics-for-makeup.jpg?s=1024x1024&w=is&k=20&c=wGmovDLR3scv2NWrha6VGUO82qn_cWwxBh1ljemwo-0=" />
               </IconButton>
             </Tooltip>
             <Menu
@@ -151,7 +181,9 @@ function Navbar() {
                 <MenuItem key={setting} onClick={handleCloseUserMenu}>
                   <Typography textAlign="center">{setting}</Typography>
                 </MenuItem>
+               
               ))}
+              
             </Menu>
           </Box>
         </Toolbar>
